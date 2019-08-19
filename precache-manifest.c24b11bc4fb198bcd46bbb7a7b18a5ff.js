c56bc813036a72b18a1e721ffe5d00fcd0009b55
Ed Sutherland <ed@upstatenypolitics.com>
2019-08-19 11:07:40 -0400

    Updates

---
+++ precache-manifest.c24b11bc4fb198bcd46bbb7a7b18a5ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31c7463da0346ad3c8ecf540cbacb99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be3f2ab997eea3e58145",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f92fa4c309fb8045d932",
+    "url": "/static/js/2.f8e8e081.chunk.js"
+  },
+  {
+    "revision": "be3f2ab997eea3e58145",
+    "url": "/static/js/main.3290c6e4.chunk.js"
+  },
+  {
+    "revision": "0aa92f87184c193e9fd4",
+    "url": "/static/js/runtime~main.e5f69ac1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
