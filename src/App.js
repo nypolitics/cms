b008a9434160bfabb58fc8d052e3f57c1c9ee612
Ed Sutherland <ed@upstatenypolitics.com>
2019-08-19 23:33:57 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 
@@ -7,11 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-      </header>
-      <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary">
         Hello World
       </Button>
+      </header>
+      
     </div>
   );
 }
