7d9dffcbcb726aca359b4704ed3448f50a6449ea
Ed Sutherland <ed@upstatenypolitics.com>
2019-08-20 00:17:00 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-      </header>
-      
-    </div>
+    <Container maxWidth="sm">
+     <Box my={4}>
+       <Typography variant="h4" component="h1" gutterBottom>
+          React Frontend
+       </Typography>
+       <Typography component="div" />
+          <Button variant="contained" color="primary">
+               Hello World
+          </Button>
+      </Box>    
+    </Container> 
   );
+
 }
 
 export default App;
